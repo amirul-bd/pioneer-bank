8a8048011da838c164527e8a402c8d577e5b43bf
amirul-bd <mdamirul655@gmail.com>
2021-07-13 22:15:42 +0600

    push deposit & withdraw section

---
+++ script.js
@@ -0,0 +1,8 @@
+let loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
+})
